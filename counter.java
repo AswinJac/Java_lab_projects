60cf4b7fad5f0e85f364bcc00ea04dcbc41cc7bf
AswinJacob <128621234+AswinJac@users.noreply.github.com>
2024-03-23 09:44:30 +0530

    Update counter.java

--- counter.java
+++ counter.java
@@ -2,71 +2,64 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-class counter extends JFrame implements ActionListener  {
-    int num=0;
-    JFrame jfr;
-    JLabel jt;
-    JButton add,minus,reset;
-    counter()
-    {
-        jfr=new JFrame();
-        jfr.setSize(320,250);
-        jfr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jfr.getContentPane().setBackground(Color.red);
-        jfr.setLayout(null);
-        jfr.setVisible(true);
-        JLabel count=new JLabel("Counter");
-        count.setForeground(Color.green);
-        count.setBounds(30, 25, 300, 50);
-        jfr.add(count);
-        jt=new JLabel("0");
-        jt.setFont(new Font("Ariel",Font.BOLD,24));
-        jt.setBounds(100,25,300,50);
-        jfr.add(jt);
-        add=new JButton("ADD");
-        add.setBounds(30,70,80,30);
-        add.setFocusable(false);
-        jfr.add(add);
-        add.addActionListener(this);
-        minus=new JButton("NEGATE");
-        minus.setBounds(140,70,110,30);
-        minus.setFocusable(false);
-        jfr.add(minus);
-        minus.addActionListener(this);
-        reset=new JButton("RESET");
-        reset.setBounds(80,110,80,30);
-        reset.setFocusable(false);
-        jfr.add(reset);
-        reset.addActionListener(this);
+
+public class Counter extends JFrame implements ActionListener {
+    private int count = 0;
+    private JLabel countLabel;
+
+    public Counter() {
+        setTitle("Counter");
+        setSize(320, 250);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setBackground(Color.red);
+        setLayout(null);
+
+        JLabel titleLabel = new JLabel("Counter");
+        titleLabel.setForeground(Color.green);
+        titleLabel.setBounds(30, 25, 300, 50);
+        add(titleLabel);
+
+        countLabel = new JLabel("0");
+        countLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        countLabel.setBounds(100, 25, 300, 50);
+        add(countLabel);
+
+        JButton addButton = new JButton("Add");
+        addButton.setBounds(30, 70, 80, 30);
+        addButton.setFocusable(false);
+        addButton.addActionListener(this);
+        add(addButton);
+
+        JButton negateButton = new JButton("Negate");
+        negateButton.setBounds(140, 70, 110, 30);
+        negateButton.setFocusable(false);
+        negateButton.addActionListener(this);
+        add(negateButton);
+
+        JButton resetButton = new JButton("Reset");
+        resetButton.setBounds(80, 110, 80, 30);
+        resetButton.setFocusable(false);
+        resetButton.addActionListener(this);
+        add(resetButton);
+
+        setVisible(true);
     }
-    public void actionPerformed(ActionEvent ae)
-    {
-        String s=ae.getActionCommand();
-        
-        if(s.equalsIgnoreCase("ADD"))
-        {
-            num=num+1;
-            jt.setText(String.valueOf(num));
+
+    public void actionPerformed(ActionEvent ae) {
+        String command = ae.getActionCommand();
+
+        if (command.equalsIgnoreCase("Add")) {
+            count++;
+        } else if (command.equalsIgnoreCase("Negate")) {
+            count--;
+        } else if (command.equalsIgnoreCase("Reset")) {
+            count = 0;
         }
-        else if(s.equalsIgnoreCase("Negate"))
-        {
-            num=num-1;
-            jt.setText(String.valueOf(num));
-        }
-        else if(s.equalsIgnoreCase("Reset"))
-        {
-            num=0;
-            jt.setText(String.valueOf(num));
-        }
-     }
+
+        countLabel.setText(String.valueOf(count));
+    }
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable(){
-        public void run()
-        {
-            new counter();
-        }});
+        SwingUtilities.invokeLater(Counter::new);
     }
-
-
-    
 }
